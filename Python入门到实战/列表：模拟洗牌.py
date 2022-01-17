7fc2076547eaaf2c1b2f901e36ee226120fda858
potato <tzu-mingliu@qq.com>
2022-01-17 21:07:07 +0800

    1.1

---
+++ Python入门到实战/列表：模拟洗牌.py
@@ -0,0 +1,8 @@
+# == Coding: UTF-8 ==
+# @name              
+# @version          1.0
+# @author           -LIULONG
+# @description      
+# @GiteeWarehouse   https://gitee.com/liu-long068/python
+# @editsession      
+# ====/******/=====
